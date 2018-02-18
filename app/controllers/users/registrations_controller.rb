e979d7bfb3bc4fdb9c94b691d94c50fdd044c752
Thomas William Page <tom.w.page@gmail.com>
2018-02-18 01:26:16 +0100

    Add name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,12 +3,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password,
+  params.require(:user).permit(:name, :email, :password,
                                :password_confirmation, :phone_number)
   end
 
   def account_update_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password,
+  params.require(:user).permit(:name, :email, :password,
                                :password_confirmation, :current_password,
                                :phone_number)
   end
